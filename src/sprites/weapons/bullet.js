d8d1598b8f5fa732024aadd0998c1e509c846494
mars <mars@huoban.com>
2016-10-12 17:32:03 +0800

    init custom weapon class

--- src/sprites/weapons/bullet.js
+++ src/sprites/weapons/bullet.js
@@ -1,8 +1,49 @@
+import Weapon from './weapon'
 
-export default class Bullet extends Phaser.Weapon {
-  constructor(game) {
-    super(game)
+class _Bullet extends Phaser.Sprite {
+  constructor(game, x, y, key, speed) {
+    super(game, x, y, key)
 
-    this.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS
+    this.active = false
+    this.speed = speed
+    this.direction = 0
+  }
+
+  get active() {
+    return this._active
+  }
+
+  set active(v) {
+    this._active = v
+    this.alpha = v ? 1:0
+  }
+
+  move(ms) {
+
+  }
+}
+
+export default class Bullet extends Phaser.Group {
+  constructor(game, key, bufferSize=30, bulletSpeed=400, fireRate=60) {
+    super(gamem, key)
+
+    this.bulletsBuffer = []
+    for(let i=0; i<bufferSize; i++) {
+      let b = new _Bullet(game, 0, 0, key, bulletSpeed)
+      this.bulletsBuffer.push(b)
+      this.addChild(b)
+    }
+    this.bulletAngleOffset = 0
+    this.fireAngleDegree = 0
+    this.bulletSpeed = bulletSpeed
+    this.fireRate = fireRate
+  }
+
+  fire() {
+
+  }
+
+  _getCurrentAvailableBulletFromBuffer() {
+
   }
 }
---
+++ src/sprites/weapons/weapon.js
@@ -0,0 +1,9 @@
+export default class Weapon extends Phaser.Group {
+  constructor(game, key) {
+    super(gamem, key)
+  }
+
+  fire() {
+
+  }
+}
